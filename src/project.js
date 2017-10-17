a8837eab385593d4595ae01c68a9443b5740963e
kbeske <beske.k@husky.neu.edu>
2017-10-17 16:10:26 -0400

    added new changes but getting NsN error

--- src/project.js
+++ src/project.js
@@ -1,25 +1,25 @@
 function Project() {
-    this.iterations = new Array();
+   	this.iterations = new Array();
 };
 
 Project.prototype.addIteration = function(iteration) {
-	iterations.appendChild(Iteration);
+	this.iterations.push(iteration);
 };
 
 Project.prototype.velocity = function() {
 	  var totalPoints = 0;
-      if (iterations.length < 3) {
-        for (var i = 0; i < iterations.length; i++) {
-          totalPoints += iterations[i];
-        };
-        return totalPoints / iterations.length;
-      };
+      if (this.iterations.length < 3) {
+        for (var i = 0; i < this.iterations.length; i++) {
+          totalPoints += this.iterations[i];
+        }
+        return totalPoints / this.iterations.length;
+      }
       else {
-        totalPoints += iterations[iterations.length - 1];
-        totalPoints += iterations[iterations.length - 2];
-        totalPoints += iterations[iterations.length - 3];
-        return totalPoints / 3;
-      };
+        totalPoints += this.iterations[this.iterations.length - 1];
+        totalPoints += this.iterations[this.iterations.length - 2];
+        totalPoints += this.iterations[this.iterations.length - 3];
+      }
+      return totalPoints / 3;
 };
 
 
