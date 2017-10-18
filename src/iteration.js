7ffb118e563935f0f223da263a594f6a845ba5f4
kbeske <beske.k@husky.neu.edu>
2017-10-18 19:32:48 -0400

    ready to submit

--- src/iteration.js
+++ src/iteration.js
@@ -1,12 +1,15 @@
 function Iteration() {
+  //keeps track of the points in this iteration
   this.points = 0;
 };
 
 Iteration.prototype.addPoints = function(points) {
+	//adds points to the points field
 	this.points += points;
 };
 
 Iteration.prototype.totalPoints = function() {
+	//returns all the points added to this iteration
 	return this.points;
 };
 
--- src/project.js
+++ src/project.js
@@ -1,23 +1,29 @@
 function Project() {
    	this.iterations = [];
+   	//stores all iterations added
 };
 
 Project.prototype.addIteration = function(iteration) {
 	this.iterations.push(iteration);
+	//adds an iteration to the iterations array
 };
 
 Project.prototype.velocity = function() {
+	  //keeps track of the 3 (or less) most recent projects
 	  var totalPoints = 0;
 	  var size = this.iterations.length;
+	  //takes care of zero case
 	  if (size == 0) {
 	  	return 0;
 	  }
+	  //takes care of less than 3 iterations in the project
       else if (size < 3) {
         for (var i = 0; i < size; i++) {
           totalPoints += this.iterations[i].totalPoints();
         }
         return totalPoints / size;
       }
+      //adds the last 3 iteration points and gets the average for the velocity
       else {
         totalPoints += (this.iterations[size - 1]).totalPoints();
         totalPoints += (this.iterations[size - 2]).totalPoints();
